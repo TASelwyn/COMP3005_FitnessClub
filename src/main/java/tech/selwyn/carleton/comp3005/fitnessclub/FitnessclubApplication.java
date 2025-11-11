b9e68b4a1c2112f882f934dc89819db611ba6be0
Thomas Selwyn <thomas@selwyn.tech>
2025-11-11 17:44:55 -0500

    Add spring initializr project

---
+++ src/main/java/tech/selwyn/carleton/comp3005/fitnessclub/FitnessclubApplication.java
@@ -0,0 +1,13 @@
+package tech.selwyn.carleton.comp3005.fitnessclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/selwyn/carleton/comp3005/fitnessclub/FitnessclubApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.selwyn.carleton.comp3005.fitnessclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
