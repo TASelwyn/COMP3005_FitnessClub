cfa7d29706ecf1c084ea6fd307a8d2a680053438
Thomas Selwyn <thomas@selwyn.tech>
2025-11-11 19:20:06 -0500

    Add initial entity mapping

---
+++ src/main/java/tech/selwyn/carleton/comp3005/fitnessclub/model/Member.java
@@ -0,0 +1,24 @@
+package tech.selwyn.carleton.comp3005.fitnessclub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "members")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
