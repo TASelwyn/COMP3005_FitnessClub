26035193add6523e1e11e723c2c37f1c8f538e6c
Thomas Selwyn <thomas@selwyn.tech>
2025-11-29 19:33:10 -0500

    Add validation error to handler

--- src/main/java/tech/selwyn/carleton/comp3005/fitnessclub/exception/GlobalExceptionHandler.java
+++ src/main/java/tech/selwyn/carleton/comp3005/fitnessclub/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package tech.selwyn.carleton.comp3005.fitnessclub.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -18,6 +20,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<Map<String, Object>> handleIllegalState(IllegalStateException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error", e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException  e) {
+        Map<String, Object> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(Exception.class)
