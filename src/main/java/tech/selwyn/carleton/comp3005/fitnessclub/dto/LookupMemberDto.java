bbe1ce10bd673acae77cecb921a40c8c293c5e17
Thomas Selwyn <thomas@selwyn.tech>
2025-12-01 23:16:48 -0500

    Remove unnecessary

--- src/main/java/tech/selwyn/carleton/comp3005/fitnessclub/dto/LookupMemberDto.java
+++ src/main/java/tech/selwyn/carleton/comp3005/fitnessclub/dto/LookupMemberDto.java
@@ -1,6 +1,5 @@
 package tech.selwyn.carleton.comp3005.fitnessclub.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record LookupMemberDto(
